0795ce3a68df0773c3266ee819dc253afea1a06e
Fredrik Persen Fostvedt <fpfostvedt@gmail.com>
2013-11-19 03:12:40 -0800

    removed whitespaces

--- lib/twilm/crawl.rb
+++ lib/twilm/crawl.rb
@@ -39,7 +39,6 @@ module Twilm
 			end
 		end
 
-
 		def uncrawled_movie
 			Movie.one_with_less_tweets_than MAX_TWEETS_PER_MOVIE
 		end
@@ -58,8 +57,6 @@ module Twilm
 		def tweet_body_from_tweet_node(node)
 			node.xpath(".//p[@class='js-tweet-text tweet-text']/text()").to_s rescue nil
 		end
-
-
   	end
 end
 
