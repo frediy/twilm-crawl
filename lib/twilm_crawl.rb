e5020be8677bd1af758a5f583fa5685106caba1c
Fredrik Persen Fostvedt <fpfostvedt@gmail.com>
2013-11-19 22:38:17 -0800

    removed unnecessary requires and moved version out of twilm_crawl

--- lib/twilm_crawl.rb
+++ lib/twilm_crawl.rb
@@ -1,15 +1,12 @@
-VERSION = "0.0.1"
-
 require 'require_all'
-
-require 'anemone'
-require 'mongo'
-require 'mongo_mapper'
 
 module TwilmCrawl
 	require_all 'twilm_crawl/models'
+	require_all 'twilm_crawl/helpers'
 
 	class Crawl
+		include ScrapeHelper
+
 		def initialize
 			MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
 			MongoMapper.database = "netflix"
---
+++ lib/twilm_crawl/version.rb
@@ -0,0 +1,3 @@
+module TwilmCrawl
+	VERSION = "0.0.1"
+end
